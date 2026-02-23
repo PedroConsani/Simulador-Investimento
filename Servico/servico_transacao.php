915bc35dd1c3e22c05349540d04413671d5f72c5
PedroConsani <a34325@alunos.ipca.pt>
2026-02-23 18:38:53 +0000

    implementa sistema de procura de acoes por nome | consome API

---
+++ Servico/servico_transacao.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="nome_acao">Símbolo da Ação:</label>
+        <input type="text" name="nome_acao" id="nome_acao">
+        <button type="submit">Verificar</button>
+    </form>
+    <?php 
+        require_once __DIR__ . "/../../config/config.php";
+        if(isset($_POST["nome_acao"])){
+            $acao = $_POST["nome_acao"];
+            $url = "https://financialmodelingprep.com/stable/profile?symbol=$acao&apikey=".API_KEY;
+            $response = file_get_contents($url); //Requisição get
+    // print_r($response);
+            $dado = json_decode($response,true); //Transforma JSON em array php
+            print_r($dado);
+        }
+
+    
+    ?>
+
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login.php" method = "post">
+        <label for="username">Login: </label>    
+        <input type="text" name = "username" id = "username">
+        <label for="senha">Senha: </label>
+        <input type="text" name="senha" id="senha">
+        <input type="submit">
+    </form>
+</body>
+</html>
